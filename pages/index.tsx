35fdd3bf82eb42a98385fe098eabb09b392726c8
Victor Pontis <victor@pont.is>
2020-07-31 11:05:25 -0700

    temp commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import {NativePaymentButton} from "../components/NativePaymentButton";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import React from "react";
+import { NativePaymentButton } from "../components/NativePaymentButton";
 
+const stripePublicKey = 'pk_live_aiaR8bh0yCqQqHVYcNXcEDkq",';
+const stripePromise = loadStripe(stripePublicKey);
 const IndexPage = () => (
   <div>
     Hi
-    <NativePaymentButton />
+    <Elements stripe={stripePromise}>
+      <NativePaymentButton />
+    </Elements>
   </div>
-)
+);
 
-export default IndexPage
+export default IndexPage;

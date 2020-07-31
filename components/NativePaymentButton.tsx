77140533aa707611fa03bddb868de76faeaa616c
Victor Pontis <victor@pont.is>
2020-07-31 11:17:10 -0700

    temp commit

--- components/NativePaymentButton.tsx
+++ components/NativePaymentButton.tsx
@@ -1,13 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 import { PaymentRequestButtonElement } from "@stripe/react-stripe-js";
 import { loadStripe, PaymentRequest } from "@stripe/stripe-js";
 import React, { useEffect, useState } from "react";
 
-const eventApiId = 'evt-9FLb0YWFrpW3Rwn'
-const currency = 'usd';
+const eventApiId = "evt-9FLb0YWFrpW3Rwn";
+const currency = "usd";
 const amountCents = 1000;
-const stripeAccountId = 'acct_1GdONDIK9ODWQCg3';
-const stripePublicKey = 'pk_live_aiaR8bh0yCqQqHVYcNXcEDkq';
+const stripeAccountId = "acct_1GdONDIK9ODWQCg3";
+const stripePublicKey = "pk_live_aiaR8bh0yCqQqHVYcNXcEDkq";
 
 export const useNativePay = (): {
   isLoading: boolean;
@@ -26,9 +26,9 @@ export const useNativePay = (): {
       });
 
       if (!stripeConnect) {
-        console.error('fuck no connect')
-        setIsLoading(false)
-        return
+        console.error("fuck no connect");
+        setIsLoading(false);
+        return;
       }
 
       const pr = stripeConnect.paymentRequest({
@@ -63,40 +63,43 @@ export const useNativePay = (): {
 };
 
 export const NativePaymentButton = () => {
-  const {paymentRequest, isLoading} = useNativePay()
-
+  const { paymentRequest, isLoading } = useNativePay();
 
   if (isLoading) {
-    return <div>Loading</div>
+    return <div>Loading</div>;
   }
 
-  if (!paymentRequest ) {
-    return <div>No payment request</div>
+  if (!paymentRequest) {
+    return <div>No payment request</div>;
   }
 
   const collectPayment = async () => {
-    const { client_secret } = await axios.post(
+    const {
+      data: { client_secret },
+    } = await axios.post(
       "https://api.zmurl.com/payments/start-payment-intent",
       {
         payment_method_id: "payment-request-api",
         event_api_id: eventApiId,
-        email: 'victor+apple@pont.is',
-        payment_type: 'registration',
+        email: "victor+apple@pont.is",
+        payment_type: "registration",
         registration_answers: null,
         amount_cents: amountCents,
       }
     );
+
     paymentRequest.on("paymentmethod", async (ev) => {
       const stripeConnect = await loadStripe(stripePublicKey, {
         stripeAccount: stripeAccountId,
       });
 
       if (!stripeConnect) {
-        console.error('why no connect');
+        console.error("why no connect");
         return;
       }
 
       // Confirm the PaymentIntent without handling potential next actions (yet).
+      console.log("starting with ", client_secret, ev.paymentMethod.id);
       const { error: confirmError } = await stripeConnect.confirmCardPayment(
         client_secret,
         { payment_method: ev.paymentMethod.id },
@@ -107,18 +110,19 @@ export const NativePaymentButton = () => {
         // Report to the browser that the payment failed, prompting it to
         // re-show the payment interface, or show an error message and close
         // the payment interface.
+        console.error("fail", confirmError);
         ev.complete("fail");
         return;
       }
 
       // Report to the browser that the confirmation was successful, prompting
       // it to close the browser payment method collection interface.
+      console.error("success", confirmError);
       ev.complete("success");
 
       // TODO: Stripe docs have another confirmCardPayment here, do we need that?
     });
   };
-
 
   return (
     <PaymentRequestButtonElement
